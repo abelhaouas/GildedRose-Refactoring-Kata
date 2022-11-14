2671c7fa278991658b4c5a835ce08aaf4fd2d588
Abderrahmane Belhaouas <abelhaouas@gmail.com>
2022-11-14 20:28:42 +0100

    respect access to property with getters and setters in java oop

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,51 +12,51 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE.getName())
-                    && !items[i].name.equals(BACKSTAGE_PASSES.getName())) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS.getName())) {
-                        items[i].quality = items[i].quality - 1;
+            if (!items[i].getName().equals(AGED_BRIE.getName())
+                    && !items[i].getName().equals(BACKSTAGE_PASSES.getName())) {
+                if (items[i].getQuality() > 0) {
+                    if (!items[i].getName().equals(SULFURAS.getName())) {
+                        items[i].setQuality(items[i].getQuality() - 1);
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (items[i].getQuality() < 50) {
+                    items[i].setQuality(items[i].getQuality() + 1);
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES.getName())) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (items[i].getName().equals(BACKSTAGE_PASSES.getName())) {
+                        if (items[i].getSellIn() < 11) {
+                            if (items[i].getQuality() < 50) {
+                                items[i].setQuality(items[i].getQuality() + 1);
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (items[i].getSellIn() < 6) {
+                            if (items[i].getQuality() < 50) {
+                                items[i].setQuality(items[i].getQuality() + 1);
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(SULFURAS.getName())) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!items[i].getName().equals(SULFURAS.getName())) {
+                items[i].setSellIn(items[i].getSellIn() - 1);
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE.getName())) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES.getName())) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS.getName())) {
-                                items[i].quality = items[i].quality - 1;
+            if (items[i].getSellIn() < 0) {
+                if (!items[i].getName().equals(AGED_BRIE.getName())) {
+                    if (!items[i].getName().equals(BACKSTAGE_PASSES.getName())) {
+                        if (items[i].getQuality() > 0) {
+                            if (!items[i].getName().equals(SULFURAS.getName())) {
+                                items[i].setQuality(items[i].getQuality() - 1);
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (items[i].getQuality() < 50) {
+                        items[i].setQuality(items[i].getQuality() + 1);
                     }
                 }
             }
